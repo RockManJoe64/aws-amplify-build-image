e60e372b3e0f7a2e780ce3e47e7e1231d097b90b
Jose Chavez <jose.e.chavez@gmail.com>
2022-10-01 20:29:34 -0500

    Fixed docker image build; deleted trigger build

--- amplify-ruby-build/trigger-build.sh
+++
@@ -1,4 +0,0 @@
-# Trigger build on AWS Amplify via webhook
-
-curl -X POST -d {} "https://webhooks.amplify.us-east-2.amazonaws.com/prod/webhooks?id=ca336560-dc1d-45da-b8ec-dbc1004a91d7&token=LpFIHXuiknX3EfiocDOabs0p3f9B8KCy3UWC4QqhI&operation=startbuild" -H "Content-Type:application/json"
-
