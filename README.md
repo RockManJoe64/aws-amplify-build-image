6a91119b6748e8a1a612d6e26ae5089474444189
Jose Chavez <jose.e.chavez@gmail.com>
2023-06-04 14:45:05 -0700

    Building image with ruby version 3.2.2

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Custom build images based on Amazon Linux for use with AWS Amplify.
 
 ## Current Images
 
-- **amplify-ruby-build** - Used to build latest Jekyll sites on AWS Amplify using ruby ~> 2.7
+- **amplify-ruby-build** - Used to build latest Jekyll sites on AWS Amplify using ruby ~> 3.2
--- amplify-ruby-build/build.sh
+++ amplify-ruby-build/build.sh
@@ -1,8 +1,9 @@
 # Build and push the docker image to AWS ECR
+RUBY_VERSION="3.2.2"
 IMAGE_NAME="amplify-ruby-build"
 ECR_REPO="public.ecr.aws/z8u1m2l7/aptdev/amplify-ruby-build"
 
-docker build -t $IMAGE_NAME .
+docker build --build-arg RUBY_VERSION=$RUBY_VERSION -t $IMAGE_NAME .
 
 IMAGE_ID=`docker images $IMAGE_NAME --format '{{.ID}}'`
 
